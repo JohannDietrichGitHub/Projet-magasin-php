9cec4a172d5c705d3f1c838f85d2128531e409f6
jojohub69 <jodiet@hotmail.fr>
2022-10-19 18:09:32 +0200

    Début de création de page pour la modification d'articles

--- php/modificationarticle.php
+++ php/modificationarticle.php
@@ -16,3 +16,59 @@ session_start();
   ?>
 </header>
 <body>
+  <center>
+  <br><br>
+    <label>Sélectionnez un article a modifier</label>
+  <br>
+  <form class="tablemod" method="POST">
+    <select name="select" class="form-select" aria-label="Default select example">
+      <option selected>Articles </option>
+      <?php
+       require "connection.php";
+       $data = $conn->query("SELECT * FROM articles")->fetchAll();
+       foreach ($data as $row) 
+       {
+         echo "<option> $row[nom] </option>";
+       }
+      ?>
+    </select>
+    <br><br><button type="submit" class="btn btn-primary">Selectionner</button>
+  </form>
+  <?php 
+
+
+  if (isset($_POST['select'])) 
+  {
+    echo "Article choisi : ".$_POST['select'];
+    $nom = $row['nom']; /* Dernier article et pas article choisi */
+    $reference = $row['reference'];
+    $prix = $row['prix_ht'];
+    $nouv = $row['nouveaute'];
+  }
+  ?>
+
+    <br><br><br><br><br>
+    <label>Modifier l'article</label>
+    <br><br>
+    <form class="table" method="POST">
+      <div class="form-group">
+        <label for="nom">Nom</label>
+        <input type="text" class="form-control" name="nom" id="nom"  placeholder="Entrez le nom de l'article" value="<?php echo $nom ?>" required> 
+      </div>
+      <div class="form-group">
+        <label for="reference">Reference</label>
+        <input type="text" class="form-control" name="reference" id="reference"  placeholder="Entrez la référence" required>
+      </div>
+      <div class="form-group">
+        <label for="prix">Prix</label>
+        <input type="text" class="form-control" name="prix" id="prix" placeholder="Entrez le prix" required>
+      </div>
+      <div class="form-check">
+        <input type="checkbox" class="form-check-input" name="nouv" id="nouv">
+        <label class="form-check-label" for="nouv">Nouveauté ?</label>
+      </div>
+      <br>
+      <button type="submit" class="btn btn-primary">Modifier</button>
+    </form>
+  </center>
+</body>
