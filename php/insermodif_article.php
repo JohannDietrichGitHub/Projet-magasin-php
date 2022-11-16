e7254df6fd1ab99426148aebfbb2f2beebda9290
jojohub69 <jodiet@hotmail.fr>
2022-11-16 17:58:13 +0100

    bugfix de la modification d'article

--- php/insermodif_article.php
+++ php/insermodif_article.php
@@ -1,4 +1,8 @@
-<?php
+<?php       
+session_start();
+
+require_once "connection.php";
+require_once "fonctions.php";
     // Modification de l'article dans la base de donnée
 if(!empty($_POST)){
   modification_article($conn);
