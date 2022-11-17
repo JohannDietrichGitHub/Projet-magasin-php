767d558aa83ea04ad2063c257925d0ba1dcf7ee8
jojohub69 <jodiet@hotmail.fr>
2022-11-17 08:46:45 +0100

    Simplification du code du header

--- php/supressionarticle.php
+++ php/supressionarticle.php
@@ -1,22 +1,4 @@
-<?php
-// Initialize the session
-session_start();
-
-
-?>
-<head> 		
-    <meta charset="utf-8" /> <!--encodage en utf8-->
-	<title>Contact</title>
-    <link rel="stylesheet" href="..\css\css.css"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="..\JS\js.js"></script>
-</head>
-
-<header>
-  <?php
-    require_once ('navbar.php');
-  ?>
-</header>
+<?php require_once('header.php'); ?>
 <body>
 <center>
   <br><br>
